5a0a7fde1c744d2a7195f91a56e23345ce93d47c
NobelAfin <nobelarmy@outlook.com>
2023-01-11 19:53:10 -0600

    ssh key 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_004120) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_075432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 end
