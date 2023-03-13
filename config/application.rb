8c46113214ad376350b155fac6b10d409643b173
NobelAfin <nobelarmy@outlook.com>
2023-03-12 20:38:58 -0600

    recuperar contraseña

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module NoblidreamOri
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
