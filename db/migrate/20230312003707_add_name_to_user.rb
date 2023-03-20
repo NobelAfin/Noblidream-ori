cc2cf444ebe606d90dbec3d7616209c1915014cf
NobelAfin <nobelarmy@outlook.com>
2023-03-20 00:58:31 -0600

    unique not working

--- db/migrate/20230312003707_add_name_to_user.rb
+++ db/migrate/20230312003707_add_name_to_user.rb
@@ -4,15 +4,13 @@ class AddNameToUser < ActiveRecord::Migration[7.0]
     add_column :users, :surname, :string, limit: 20, null: false
     add_column :users, :mother_surname, :string, limit: 20
     add_column :users, :birthdate, :date, null: false
-    add_column :users, :phone, :string, limit: 16, null: false
-    add_index :users, :phone, unique: true
+    add_column :users, :phone, :string, limit: 16, null: false, unique: true
     add_column :users, :address, :binary, limit: 20, null: false
     add_column :users, :exterior_number, :string, limit: 8, null: false
     add_column :users, :interior_number, :string, limit: 8
     add_column :users, :zip_code, :string, limit: 8
     add_column :users, :last_login, :timestamp
     add_column :users, :num_of_hab, :integer, limit: 4
-    add_column :users, :username, :binary, limit: 20, null: false
-    add_index :users, :username, unique: true
+    add_column :users, :username, :binary, limit: 20, null: false, unique: true
   end
 end
